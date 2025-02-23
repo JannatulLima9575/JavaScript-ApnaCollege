32377ba7ea18f2e1a85a27f160d64256c9332967
Jannatul <limajannat1675@gmail.com>
2025-02-23 21:23:26 +0600

    added problem solving2

---
+++ chapter1/problemSolving2.js
@@ -0,0 +1,10 @@
+// Problem-2: Create a const object called "profile" to store information shown in the PictureInPictureEvent. 
+const profile = {
+    usename: "@Jannatul Lima",
+    bio: "Web Developer",
+    Location: "Dhaka, Bangladesh",
+    followers: 569 +"k",
+    following: 4,
+    isFollowed: true,
+};
+console.log(profile);
--- chapter1/variablesAndDatatype.js
+++ chapter1/variablesAndDatatype.js
@@ -9,6 +9,9 @@ console.log(student); //print for all values
 console.log(student.name); //print for selected key value
 console.log(student['age']) // print for selected key value
 
+console.log(typeof student["isPassed"]); //print for data type of selected key value
+
+
 student.name = 'Jannatul Lima'; //updated value
 console.log(student.name); //print for updated value
 
