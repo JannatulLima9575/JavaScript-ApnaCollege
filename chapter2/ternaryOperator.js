8af9d16448b1788a30c80d75db75bfee1112d131
Jannatul <limajannat1675@gmail.com>
2025-02-23 22:28:42 +0600

    added ternary operator

--- chapter2/ternaryOperator.js
+++ chapter2/ternaryOperator.js
@@ -2,4 +2,10 @@
 // condition ? true output : false output
 let age = 20;
 let result = age > 18 ? "adult" : "not adult"; //similar, compact if-else
-console.log(result);
+console.log(result); //adult
+
+// nested ternary operator 
+let age2 = 20;
+let result2 = age2 > 18 ? "adult" :
+    age2 < 18 ? "child" : "Not adult";
+    console.log(result2);
