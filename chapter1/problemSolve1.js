f7b5f4140ef07cb6a29f23f4a629d453d42d9b7e
Jannatul <limajannat1675@gmail.com>
2025-02-23 21:09:13 +0600

    added problem solving1

---
+++ chapter1/problemSolve1.js
@@ -0,0 +1,10 @@
+// problem-1 : Create a const object called "product" to store information shown in the picture.
+const product = {
+    name: "Ball Pen",
+    rating: 4.5,
+    offer: "Buy 1 Get 1 Free",
+    price: 10,
+    stock: 100,
+};
+console.log(product);
+console.log(typeof product);
