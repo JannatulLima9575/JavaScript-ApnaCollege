249feff4deb46f3198075000b7803c6000283ed4
Jannatul <limajannat1675@gmail.com>
2025-02-23 21:00:06 +0600

    added chapter1 variables & datatype

---
+++ chapter1/variablesAndDatatype.js
@@ -0,0 +1,16 @@
+// # About objects
+const student = {
+    name: 'Lima',
+    age: 22,
+    cgpa: 3.75,
+    isPassed: true,
+};
+console.log(student); //print for all values
+console.log(student.name); //print for selected key value
+console.log(student['age']) // print for selected key value
+
+student.name = 'Jannatul Lima'; //updated value
+console.log(student.name); //print for updated value
+
+
+
