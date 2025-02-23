7640e6b94da5018cc66a71db4dc142c02710a003
Jannatul <limajannat1675@gmail.com>
2025-02-24 00:16:31 +0600

    added loops method

---
+++ chapter3/do-while-loop.js
@@ -0,0 +1,9 @@
+// # basic syntax of do-while loop 
+// do {do something} while (condition)
+
+let i = 20;  //initialize the variable
+do {
+    console.log(i);
+    // i++; increment the variable
+}
+while(i <=10); //stopping condition
---
+++ chapter3/while-loop.js
@@ -0,0 +1,8 @@
+// # Basic structure of while loop :
+// while (condition) {do something}
+
+let i = 1; //initialize the variable
+while(i <=10) { //stopping condition
+    console.log(i);
+    i++; // increment the variable
+}
